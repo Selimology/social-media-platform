d0625328148c5a7ceb3feb55ef67a1a799c24596
Kamil Ertekin <kertekin19@epoka.edu.al>
2022-09-12 21:48:10 +0200

    feat: sidebar section

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,7 +1,67 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { NextPage } from 'next';
+import { useRouter } from 'next/router';
+import Link from 'next/link';
+import GoogleLogin from 'react-google-login';
+import { MdOutlineCancel, MdOutlineMenu, MdHomeFilled } from 'react-icons/md';
+import { Discover, Recommended, Footer } from '.';
 
 const Sidebar = () => {
-  return <div>Sidebar</div>;
+  const [toggleSidebar, setToggleSidebar] = useState(true);
+  const loggedIn = false;
+  const normalLink =
+    'flex items-center gap-3  justify-center xl:justify-start cursor-pointer px-4 py-2 text-xl xl:text-md mb-4 xl:mb-0 border-2 bg-black text-white rounded ease-in-out transform duration-100 hover:bg-white hover:text-black xl:border-0 xl:bg-white xl:text-black xl:hover:tracking-wider ';
+  return (
+    <div>
+      <div
+        className="block xl:hidden m-2 ml-4 mt-3 text-xl"
+        onClick={() => setToggleSidebar((prev) => !prev)}
+      >
+        {toggleSidebar ? <MdOutlineCancel /> : <MdOutlineMenu />}
+      </div>
+      {toggleSidebar && (
+        <div className=" flex flex-col w-20 mt-2 border-gray-100 border-l-2 xl:w-[400px] p-2 mb-12 justify-start ">
+          <div className="xl:border-b-2 border-gray-200 xl:pb-4 xl:pr-4">
+            <Link href="/">
+              <div className={normalLink}>
+                <p>
+                  <MdHomeFilled />
+                </p>
+                <span className="text-xl hidden xl:block"> For You</span>
+              </div>
+            </Link>
+          </div>
+          {!loggedIn && (
+            <div className="px-2 py-4 hidden xl:block">
+              <p className="text-gray-400 text-lg">
+                Log in to like and comment on videos
+              </p>
+              <div className="pr-4">
+                <GoogleLogin
+                  clientId=""
+                  onSuccess={() => {}}
+                  onFailure={() => {}}
+                  cookiePolicy={'single_host_origin'}
+                  render={(renderProps) => (
+                    <button
+                      className="text-gray-600 font-bold px-4 py-2 rounded  border-gray-400 border-[2px]   w-full mt-4  hover:bg-gray-100 cursor-pointer"
+                      disabled={renderProps.disabled}
+                      onClick={renderProps.onClick}
+                    >
+                      Login
+                    </button>
+                  )}
+                />
+              </div>
+            </div>
+          )}
+          <Discover />
+          <Recommended />
+          <Footer />
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default Sidebar;
