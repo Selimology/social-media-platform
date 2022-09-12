d5a42d5e7d3018aee3f930d2f193bd377ee9be75
Kamil Ertekin <kertekin19@epoka.edu.al>
2022-09-12 21:49:46 +0200

    feat[layout]: setup main layout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
 import { useState, useEffect } from 'react';
+import { Navbar, Sidebar } from '../components';
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   const [checkSSR, setcheckSSR] = useState(true);
@@ -13,3 +14,19 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
   //don't show components if isSSR is true
   if (checkSSR) return null;
 
+  return (
+    <div>
+      <Navbar />
+      <div className="flex gap-4 md:gap-16">
+        <div className=" mt-4 flex overflow-auto h-[84vh] video flex-1 flex-col gap-8 ">
+          <Component {...pageProps} />
+        </div>
+        <div className="overflow-hidden xl:hover:overflow-auto h-[90vh]">
+          <Sidebar />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default MyApp;
