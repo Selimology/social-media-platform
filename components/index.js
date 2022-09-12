9a61e0bba84235b72aff845dea5516fe501d8849
Kamil Ertekin <kertekin19@epoka.edu.al>
2022-09-12 21:49:07 +0200

    chore: export multiple components

--- components/index.js
+++ components/index.js
@@ -1,2 +1,5 @@
 export { default as Navbar } from './Navbar';
 export { default as Sidebar } from './Sidebar';
+export { default as Recommended } from './Recommended';
+export { default as Footer } from './Footer';
+export { default as Discover } from './Discover';
