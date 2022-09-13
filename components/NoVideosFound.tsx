5b19ea0d235b5775dc585d1195ada54cbca9668a
Kamil Ertekin <kertekin19@epoka.edu.al>
2022-09-13 19:10:10 +0200

    feat: add No Videos Found component

---
+++ components/NoVideosFound.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+interface Props {
+  text: string;
+}
+const NoVideosFound = ({ text }: Props) => {
+  return <div>NoVideosFound</div>;
+};
+
+export default NoVideosFound;
