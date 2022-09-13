e9203361e93e7559c4dec0ec661e148a0da074b7
Kamil Ertekin <kertekin19@epoka.edu.al>
2022-09-13 13:46:57 +0200

    feat: setup api

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,21 @@
 import type { NextPage } from 'next';
+import axios from 'axios';
 
-const Home: NextPage = () => {
+export const getServerSideProps = async () => {
+  const { data } = await axios.get(`http://localhost:3000/api/posts`);
+  return {
+    props: {
+      videos: data,
+    },
+  };
+};
+
+// interface Props{
+//   videos:
+// }
+
+const Home: NextPage = ({ videos }) => {
+  console.log(videos);
   return <div>hello</div>;
 };
 
