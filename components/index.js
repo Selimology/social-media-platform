fc6d344e8b710b33a7e36c5750a41e0e66c70d74
Kamil Ertekin <kertekin19@epoka.edu.al>
2022-09-13 20:08:11 +0200

    chore: export multiple components

--- components/index.js
+++ components/index.js
@@ -3,3 +3,5 @@ export { default as Sidebar } from './Sidebar';
 export { default as Recommended } from './Recommended';
 export { default as Footer } from './Footer';
 export { default as Discover } from './Discover';
+export { default as VideoCard } from './VideoCard';
+export { default as NoVideosFound } from './NoVideosFound';
