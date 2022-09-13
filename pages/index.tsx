b4394fa05d691cd7b84e78922336666ff8a1f127
Kamil Ertekin <kertekin19@epoka.edu.al>
2022-09-13 20:09:19 +0200

    feat[main] : import video and no video available component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,7 @@
 import type { NextPage } from 'next';
 import axios from 'axios';
+import { Video } from '../types/types';
+import { NoVideosFound, VideoCard } from '../components';
 
 export const getServerSideProps = async () => {
   const { data } = await axios.get(`http://localhost:3000/api/posts`);
@@ -10,13 +12,21 @@ export const getServerSideProps = async () => {
   };
 };
 
-// interface Props{
-//   videos:
-// }
+interface Props {
+  videos: Video[];
+}
 
-const Home: NextPage = ({ videos }) => {
+const Home = ({ videos }: Props) => {
   console.log(videos);
-  return <div>hello</div>;
+  return (
+    <div className="flex flex-col gap-10 videos h-full">
+      {videos.length ? (
+        videos.map((video: Video) => <VideoCard post={video} key={video._id} />)
+      ) : (
+        <NoVideosFound text={'No Videos'} />
+      )}
+    </div>
+  );
 };
 
 export default Home;
