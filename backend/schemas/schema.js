bbafe8bc8b4933c3e165e0785e10e212359dd2c3
Kamil Ertekin <kertekin19@epoka.edu.al>
2022-09-13 13:32:33 +0200

    fix [backend]: reinstall sanity and schemas

--- backend/schemas/schema.js
+++ backend/schemas/schema.js
@@ -3,10 +3,10 @@ import createSchema from 'part:@sanity/base/schema-creator';
 
 // Then import schema types from any plugins that might expose them
 import schemaTypes from 'all:part:@sanity/base/schema-type';
+import user from './user';
 import post from './post';
 import postedBy from './postedBy';
 import comment from './comment';
-import user from './user';
 
 // Then we give our schema to the builder and provide the result to Sanity
 export default createSchema({
@@ -15,10 +15,10 @@ export default createSchema({
   // Then proceed to concatenate our document type
   // to the ones provided by any plugins that are installed
   types: schemaTypes.concat([
-    post,
-    postedBy,
     comment,
     user,
+    post,
+    postedBy,
     /* Your types here! */
   ]),
 });
