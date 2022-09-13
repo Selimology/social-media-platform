10257dbe629dd5bff728ef0cb8af989cb3dc0f1c
Kamil Ertekin <kertekin19@epoka.edu.al>
2022-09-13 13:46:03 +0200

    feat [Footer] : add links

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,28 @@
 import React from 'react';
+import { footerLinks as Links } from '../utils/constants';
+
+const FooterLinks = ({ items }: { items: string[] }) => {
+  return (
+    <div className="flex  gap-3 flex-wrap">
+      {items.map((item) => (
+        <p
+          className="text-sm text-gray-600 cursor-pointer hover:underline"
+          key={item}
+        >
+          {item}
+        </p>
+      ))}
+    </div>
+  );
+};
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="mt-5 hidden xl:block overflow-hidden">
+      <FooterLinks items={Links} />
+      <p className="text-sm  text-gray-700 mt-2">2020 &copy; Kamil Ertekin</p>
+    </footer>
+  );
 };
 
 export default Footer;
