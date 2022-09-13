a0fac105e35926540823f0a2e5f4e754d1028b32
Kamil Ertekin <kertekin19@epoka.edu.al>
2022-09-13 13:43:54 +0200

    feat [Sanity]: sanity backend to frontend client

---
+++ utils/client.tsx
@@ -0,0 +1,9 @@
+import sanityClient from '@sanity/client';
+
+export const client = sanityClient({
+  projectId: 'bp61kayz',
+  dataset: 'production',
+  apiVersion: '2022-09-13',
+  useCdn: false,
+  token: process.env.NEXT_PUBLIC_SANITY_TOKEN,
+});
