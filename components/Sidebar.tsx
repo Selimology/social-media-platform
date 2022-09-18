74ca47bd51c2630b843f1d7b4c09a65e40d61da2
Kamil Ertekin <kertekin19@epoka.edu.al>
2022-09-18 21:19:12 +0200

    feat: remove login section
    Replace it with google oauth login.

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -7,7 +7,6 @@ import { Discover, Recommended, Footer } from '.';
 
 const Sidebar = () => {
   const [toggleSidebar, setToggleSidebar] = useState(true);
-  const loggedIn = false;
   const normalLink =
     'flex items-center gap-3  justify-center xl:justify-start cursor-pointer px-4 py-2 text-xl xl:text-md mb-4 xl:mb-0 border-2 bg-black text-white rounded ease-in-out transform duration-100 hover:bg-white hover:text-black xl:border-0 xl:bg-white xl:text-black xl:hover:tracking-wider ';
   return (
@@ -19,7 +18,7 @@ const Sidebar = () => {
         {toggleSidebar ? <MdOutlineCancel /> : <MdOutlineMenu />}
       </div>
       {toggleSidebar && (
-        <div className=" flex flex-col w-20 mt-2 border-gray-100 border-l-2 xl:w-[400px] p-2 mb-6 justify-start overflow-hidden">
+        <div className=" flex flex-col w-20 mt-2 border-gray-100 border-l-2 xl:w-[400px] p-2 mb-6 justify-start overflow-hidden ">
           <div className="xl:border-b-2 border-gray-200 xl:pb-2 xl:pr-4">
             <Link href="/">
               <div className={normalLink}>
@@ -30,21 +29,7 @@ const Sidebar = () => {
               </div>
             </Link>
           </div>
-          {!loggedIn && (
-            <div className="px-2 py-4 hidden  xl:block">
-              <p className="text-gray-400 text-base">
-                Log in to like and comment on videos
-              </p>
-              <div className="pr-4">
-                <button
-                  className=" font-bold px-4 py-2 rounded  hover:border-gray-400 border-[2px]   w-full mt-2   cursor-pointer
-                      text-white  bg-black hover:bg-white hover:text-black"
-                >
-                  Login
-                </button>
-              </div>
-            </div>
-          )}
+
           <Discover />
           <Recommended />
           <Footer />
